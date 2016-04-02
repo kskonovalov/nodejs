259372bdff17dc53c2db694ed56d28d8820a04c0
Konstantin <picprofit@gmail.com>
2016-04-02 14:01:22 +0300

    homework 3.1 news from site

---
+++ 3/index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by konstantin on 2.4.16.
+ */
+'use strict';
+
+var request = require("request");
+var urlutils = require("url");
+var cheerio = require("cheerio"); //jQuery
+
+request("http://fognews.ru/", (error, response, body) => {
+   if(!error) {
+       if(response.statusCode == 200) {
+           var $ = cheerio.load(body);
+           $("article .media-body").each(function(i){
+              console.log($(this).find("em").eq(0).text() + " " +$(this).find("h4 a").eq(0).text());
+              console.log($(this).find("a.hidden-xs.hidden-sm").text());
+               console.log("Полная новость: " + $(this).find("h4 a").eq(0).attr("href") + "\n\n");
+
+               return i<2; //show three last news
+           });
+       }
+       else
+           console.log("Ошибка! Код ответа сервера " + response.statusCode);
+   }
+    else
+       console.log(error);
+});
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ node dz-3.js<br />
 node index.js --log log.txt<br />
 node blackjack.js --log log.txt<br />
 node log1.js --log log.txt<br />
+
+#nodejs-3<br />
+node index.js
