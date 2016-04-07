79e46d04f9855034ea58b127dc4a654b2898b286
Konstantin <picprofit@gmail.com>
2016-04-07 19:19:05 +0300

    get last N quotes from bash.im/abyssbest

--- 4/index.js
+++ 4/index.js
@@ -5,9 +5,63 @@
 
 const express = require("express");
 const app = express();
+const templating = require('consolidate');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
+const request = require("request");
+const cheerio = require("cheerio"); //jQuery
+const iconv = require("iconv");
+
+
+app.engine('hbs', templating.handlebars);
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
 
 app.get("/", (req, res) => {
-    res.send('Hello, Konstantin!');
+    res.render('main', {
+        title: 'Что-то странное',
+        partials: {
+            header: "header"
+        }
+    });
+});
+
+app.post('/', (req, res) =>{
+
+    var quotes = [];
+    request({
+        uri: "http://bash.im/abyssbest",
+        encoding: 'binary'
+    }, (error, response, body) => {
+        if(!error) {
+            if(response.statusCode == 200) {
+                body = new Buffer(body, 'binary');
+                var conv = new iconv.Iconv('windows-1251', 'utf8');
+                body = conv.convert(body).toString();
+
+                var $ = cheerio.load(body);
+                $(".quote").each(function(i){
+                    quotes.push($(this).find(".text").eq(0).text());
+                    return i<req.body.count-1; //show N last news
+                });
+
+                res.render('main', {
+                    title: 'Что-то смешное',
+                    count: req.body.count,
+                    quotes: quotes,
+                    partials: {
+                        header: "header"
+                    }
+                });
+
+            }
+            else
+                console.log("Ошибка! Код ответа сервера " + response.statusCode);
+        }
+        else
+            console.log(error);
+    });
 });
 
 app.listen(5000);
